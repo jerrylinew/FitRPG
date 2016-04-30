550d2e6de25c95f852695a71f4c15ffd9ef9bc13
Jerry Li <jerrylinew@gmail.com>
2016-04-30 16:57:29 -0700

    testing

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.get('/refreshdata', function(req, res){
 
     apiClient.get("/activities/date/today.json", users[user_ID].accessToken).then(function (results) {
         data["daySteps"] = results[0].summary.steps;
-        data["coins"] += data["daySteps"] - currentUser["dailyAwarded"];
+        data["coins"] = currentUser["coins"] + data["daySteps"] - currentUser["dailyAwarded"];
 
         users[user_ID]["dailyAwarded"] = data["daySteps"];
         users[user_ID]["coins"] = data["coins"];
