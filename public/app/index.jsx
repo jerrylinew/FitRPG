5dbbc6eca3322dfd0228e6006862e1809c436dff
Xinyu Yang <colin.yangxinyu@outlook.com>
2016-04-30 02:50:15 -0700

    prototype toolbar

--- public/app/index.jsx
+++ public/app/index.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
+import { Button, ButtonToolbar } from 'react-bootstrap';
+
+
+class Header extends React.Component {
+    render () {
+        /*var ButtonToolBar = ReactBootstrap.ButtonToolBar;
+         *var Button = ReactBootstrap.Button;*/
+        return (
+            <div>
+                <ButtonToolbar>
+                    <Button>Name</Button>
+                    <Button>Coins</Button>
+                    <Button>ATK</Button>
+                    <Button>DEF</Button>
+                    <Button>Help</Button>
+                    <Button>Logout</Button>
+                </ButtonToolbar>
+            </div>
+        );
+    }
+}
 
 class App extends React.Component {
     render () {
-        return <p> Hello React! </p>;
+        return (
+            <div>
+                <p> Hello React! </p>
+                <Header/>
+            </div>
+        );
     }
 }
+
 
 render(<App/>, document.getElementById('app'));
