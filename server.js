e0804ddcb54f3cc945af0086c6b5b39ce4ba276c
Jerry Li <jerrylinew@gmail.com>
2016-04-29 23:48:57 -0700

    Started server

--- server.js
+++ server.js
@@ -1,3 +1,42 @@
 /**
  * Created by jerrylinew on 4/29/16.
  */
+
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var url = require('url');
+var FitbitApiClient = require("fitbit-node");
+var redirectURL = "http://www.fitbitrpg.co/"
+
+function fullUrl(req) {
+    return url.format({
+        protocol: req.protocol,
+        host: req.get('host'),
+        pathname: req.originalUrl
+    });
+}
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/public/index.html');
+
+    var client = new FitbitApiClient("227LR8", "0c5043e5c97351930aa2a3431cb79266");
+    var authURL = client.getAuthorizeUrl("activity heartrate sleep profile weight", redirectURL);
+
+    console.log(authURL);
+    console.log(fullUrl(req));
+
+    var code = req.param('code');
+    var promise = client.getAccessToken(code, redirectURL);
+    console.log(promise);
+});
+
+app.listen(port, function(){
+    console.log('App listening on port 8080!');
+});
+
+
+
+
+
+
