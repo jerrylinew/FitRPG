d1f6911bc1bae0e2fc9642ac70c7ca7571895226
Andy Shih <andyshih12@hotmail.com>
2016-04-30 02:10:17 -0700

    try getting GET response

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ app.get('/', function(req, res){
 
     apiClient.getAccessToken(code, redirectURL).then(function(result){
         console.log(result);
+        apiClient.get("/profile.json", result.access_token).then(function (results) {
+            console.log(results[0]);
+        });
         apiClient.get("/activities/tracker/steps/date/2016-04-29/2016-04-29/1min/time/00:00/23:59.json", result.access_token).then(function (results) {
             console.log(results[0]);
         });
