c1abf19c899d1497619f234931d73bdea8ec3997
Jerry Li <jerrylinew@gmail.com>
2016-04-30 00:34:18 -0700

    Added setup to server

--- server.js
+++ server.js
@@ -7,28 +7,23 @@ var app = express();
 var port = process.env.PORT || 8080;
 var url = require('url');
 var FitbitApiClient = require("fitbit-node");
-var redirectURL = "http://www.fitbitrpg.co/"
-
-function fullUrl(req) {
-    return url.format({
-        protocol: req.protocol,
-        host: req.get('host'),
-        pathname: req.originalUrl
-    });
-}
+var redirectURL = "http://www.fitbitrpg.co/";
+var apiClient = new FitbitApiClient("227LR8", "0c5043e5c97351930aa2a3431cb79266");
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/index.html');
 
-    var client = new FitbitApiClient("227LR8", "0c5043e5c97351930aa2a3431cb79266");
-    var authURL = client.getAuthorizeUrl("activity heartrate sleep profile weight", redirectURL);
-
-    console.log(authURL);
-    console.log(fullUrl(req));
-
-    var code = req.param('code');
-    var promise = client.getAccessToken(code, redirectURL);
+    var code = req.param("code");
+    var promise = apiClient.getAccessToken(code, redirectURL);
+    console.log(code);
     console.log(promise);
+});
+
+app.get('/setup', function(req, res){
+    res.sendFile(__dirname + '/public/index.html');
+
+    var authURL = apiClient.getAuthorizeUrl("activity heartrate sleep profile weight", redirectURL);
+    res.redirect(authURL);
 });
 
 app.listen(port, function(){
