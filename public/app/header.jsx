8c4122683aafca3c8293335eb5a73fc6a6c472a4
Xinyu Yang <colin.yangxinyu@outlook.com>
2016-04-30 12:14:26 -0700

    experiment

--- public/app/header.jsx
+++ public/app/header.jsx
@@ -2,12 +2,9 @@ import React from 'react';
 import { Button, ButtonToolbar, Navbar, Nav, NavItem } from 'react-bootstrap';
 
 
-/*class Name extends React.Component {
- *
- *}*/
-
-
 export class Header extends React.Component {
+
+
     render () {
         /*var ButtonToolBar = ReactBootstrap.ButtonToolBar;
          *var Button = ReactBootstrap.Button;*/
@@ -19,13 +16,13 @@ export class Header extends React.Component {
                             <a>FitRPG</a>
                         </Navbar.Brand>
                     </Navbar.Header>
-                    <Nav>
-                        <NavItem>Name</NavItem>
-                        <NavItem>Coins</NavItem>
-                        <NavItem>ATK</NavItem>
-                        <NavItem>DEF</NavItem>
-                        <NavItem>Help</NavItem>
-                        <NavItem>Logout</NavItem>
+                    <Nav justified>
+                        <NavItem>name{/* {this.props.name} */}</NavItem>
+                        <NavItem>coins{/* {this.props.coins} */}</NavItem>
+                        <NavItem>atk{/* {this.props.attack} */}</NavItem>
+                        <NavItem>def{/* {this.props.defense} */}</NavItem>
+                        <NavItem href="">Help</NavItem>
+                        <NavItem href="">Logout</NavItem>
                     </Nav>
                 </Navbar>
             </div>
