e83e60aa0ed029f24440dcd6069a8095e8b628a9
Jerry Li <jerrylinew@gmail.com>
2016-05-01 00:01:29 -0700

    HP Bar testing

--- public/client.js
+++ public/client.js
@@ -24,6 +24,8 @@ function getParameterByName(name, url) {
         results = regex.exec(url);
     if (!results) return null;
     if (!results[2]) return '';
+    console.log(name)
+    console.log(results);
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
